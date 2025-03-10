7ffb9e6a4dfdd6ce0e72bd19b02e938b34c9e571
Everardo Torres <everardo@flexbpo.com>
2025-03-09 22:09:03 -0600

    Init gh-pages branch

---
+++ build/js/app.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",()=>{new Typed("#typed",{strings:["Developer","Artisan","Researcher"],typeSpeed:100,backSpeed:100,backDelay:500,startDelay:500,fadeOut:!0,loop:!0,smartBackspace:!0,backDelay:500,showCursor:!0})});
+//# sourceMappingURL=app.js.map
---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    var typed = new Typed("#typed", {
+        strings: [ 'Developer', 'Artisan','Researcher'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 500,
+        startDelay: 500,
+        fadeOut: true,
+        loop: true,
+        smartBackspace:true,
+        backDelay:500,
+        showCursor:true,
+
+      });
+        
+});
